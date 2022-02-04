aeb990881f62678d41fa46283157362b54574a35
JeonJunPyo <wnsvy2885@gmail.com>
2022-02-04 17:19:25 +0900

    second

---
+++ src/main/java/board/dto/BoardDTO.java
@@ -0,0 +1,20 @@
+package board.dto;
+
+import java.time.LocalDateTime;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Data;
+	@Alias("BoardDTO")
+	@Data
+	public class BoardDTO{
+	    private int boardIdx;
+	    private String title;
+	    private String contents;
+	    private int hitCnt;
+	    private String creatorId;
+	    private LocalDateTime createdDatetime;
+	    private String updaterId;
+	    private LocalDateTime updatedDatetime;
+	
+}
